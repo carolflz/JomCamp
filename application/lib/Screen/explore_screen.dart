802da1e28e5556ac848d47fd97a0d3cd7f4e8d2e
Lee Ter Qin(TQ) <92447767+jake1ee@users.noreply.github.com>
2023-12-29 01:20:44 +0800

    Category Text
    Add a Category Text widget to the explore screen

--- application/lib/Screen/explore_screen.dart
+++ application/lib/Screen/explore_screen.dart
@@ -1,4 +1,5 @@
 import 'package:application/Ressuable_widget/banner_widget.dart';
+import 'package:application/Ressuable_widget/category_text.dart';
 import 'package:application/Ressuable_widget/search_text_widget.dart';
 import 'package:application/Ressuable_widget/welcome_text_widget.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,11 +10,13 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
      children: [
        WelcomeText(),
        SizedBox(height: 10,),
        SearchText(),
-       BannerWidget()
+       BannerWidget(),
+       CategoryText(),
      ],
    ); 
   }
