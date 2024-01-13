0f95baf4094ce9f73adeec045d6f2d2b71b29343
Foo Ling Zhen <lzhen803@gmail.com>
2024-01-13 22:18:53 +0800

    solved conflict

--- application/lib/Ressuable_widget/google_map.dart
+++ application/lib/Ressuable_widget/google_map.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:geoflutterfire2/geoflutterfire2.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 
 void main() => runApp(const MyMap());
 
